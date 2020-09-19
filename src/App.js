056d8e22cd57578b02ffde82a13f2d6f964f9084
DharmarajX24 <geniusraj2002.244@gmail.com>
2020-09-19 11:16:26 +0530

    Replaced Class by Function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Path from 'path'
+import React, { useState } from 'react'; 
 import './App.css';
 import { Nav, Navbar, OverlayTrigger, Tooltip } from 'react-bootstrap'
 import { Jumbotron, Card, CardDeck } from 'react-bootstrap'
@@ -8,19 +7,13 @@ import { GitHub, MessageSquare, Twitter, Sun } from 'react-feather'
 import { getPokeData } from './handlers/getPokeData'
 import pokeball from './assets/pokeball.png'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      poke1Img: pokeball,
-      poke2Img: Path.join(__dirname, '/assets/pokeball.png'),
-      poke3Img: Path.join(__dirname, '/assets/pokeball.png')
-    }
-    this.getPokemonData() 
-  }
-  
-  async getPokemonData () {
-    this.setState({poke2Img: pokeball})
+function App() {
+  const [poke1Img, setPoke1Img] = useState(pokeball)
+  const [poke2Img, setPoke2Img] = useState(pokeball)
+  const [poke3Img, setPoke3Img] = useState(pokeball)
+
+  window.onload = async function () {
+    // this.setState({poke2Img: pokeball})
     if (!localStorage.getItem("firstVisit")) {
       console.log('First Visit')
       try {
@@ -33,146 +26,145 @@ class App extends React.Component {
       }
     } else {
       console.log('Old user')
-    } 
-  }
+    }
+  } 
 
-  render() {
-    return (
-      <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <Navbar bg="light" expand="lg" className="d-flex shadow w-100">
-          <Navbar.Brand className="font-expletus" href="#home">
-            <img
-              alt=""
-              src="/pokeball.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
+  return (
+    <div className="App" style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <Navbar bg="light" expand="lg" className="d-flex shadow w-100">
+        <Navbar.Brand className="font-expletus" href="#home">
+          <img
+            alt=""
+            src="/pokeball.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
           Pokedex
         </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-            <Nav className="justify-content-end">
-              <OverlayTrigger
-                key='bottom'
-                placement='bottom'
-                overlay={
-                  <Tooltip className="font-expletus" id={`tooltip-git`}>
-                    View Source Code
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav className="justify-content-end">
+            <OverlayTrigger
+              key='bottom'
+              placement='bottom'
+              overlay={
+                <Tooltip className="font-expletus" id={`tooltip-git`}>
+                  View Source Code
                 </Tooltip>
-                }
-              >
-                <Nav.Link className="mx-2" href="#home"><GitHub color="black" /></Nav.Link>
-              </OverlayTrigger>
-              <OverlayTrigger
-                key='bottom'
-                placement='bottom'
-                overlay={
-                  <Tooltip className="font-expletus" id={`tooltip-discord`}>
-                    Contact on Discord
+              }
+            >
+              <Nav.Link className="mx-2" href="#home"><GitHub color="black" /></Nav.Link>
+            </OverlayTrigger>
+            <OverlayTrigger
+              key='bottom'
+              placement='bottom'
+              overlay={
+                <Tooltip className="font-expletus" id={`tooltip-discord`}>
+                  Contact on Discord
                 </Tooltip>
-                }
-              >
-                <Nav.Link className="mx-2" href="#link"><MessageSquare color="black" /></Nav.Link>
-              </OverlayTrigger>
-              <OverlayTrigger
-                key='bottom'
-                placement='bottom'
-                overlay={
-                  <Tooltip className="font-expletus" id={`tooltip-twitter`}>
-                    Tweet
+              }
+            >
+              <Nav.Link className="mx-2" href="#link"><MessageSquare color="black" /></Nav.Link>
+            </OverlayTrigger>
+            <OverlayTrigger
+              key='bottom'
+              placement='bottom'
+              overlay={
+                <Tooltip className="font-expletus" id={`tooltip-twitter`}>
+                  Tweet
                 </Tooltip>
-                }
-              >
-                <Nav.Link className="mx-2" href="#link"><Twitter color="black" /></Nav.Link>
-              </OverlayTrigger>
-              <OverlayTrigger
-                key='bottom'
-                placement='bottom'
-                overlay={
-                  <Tooltip className="font-expletus" id={`tooltip-theme`}>
-                    Toggle Theme
+              }
+            >
+              <Nav.Link className="mx-2" href="#link"><Twitter color="black" /></Nav.Link>
+            </OverlayTrigger>
+            <OverlayTrigger
+              key='bottom'
+              placement='bottom'
+              overlay={
+                <Tooltip className="font-expletus" id={`tooltip-theme`}>
+                  Toggle Theme
                 </Tooltip>
-                }
-              >
-                <Nav.Link className="mx-2" href="#link"><Sun color="black" /></Nav.Link>
-              </OverlayTrigger>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+              }
+            >
+              <Nav.Link className="mx-2" href="#link"><Sun color="black" /></Nav.Link>
+            </OverlayTrigger>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-        <Jumbotron className="my-5 d-flex-col-center" style={{ width: "90%" }}>
-          <h1 className="font-expletus">Search for a Pokemon</h1>
-          <div id="div-search-poke">
-            <Form className="my-3">
-              <Form.Group controlId="formPokeName">
-                <Form.Control className="font-overpass-mono" type="text" placeholder="Pokemon Name" />
-                <Form.Text className="text-muted font-expletus">
-                  A typo or two are fine ;)
+      <Jumbotron className="my-5 d-flex-col-center" style={{ width: "90%" }}>
+        <h1 className="font-expletus">Search for a Pokemon</h1>
+        <div id="div-search-poke">
+          <Form className="my-3">
+            <Form.Group controlId="formPokeName">
+              <Form.Control className="font-overpass-mono" type="text" placeholder="Pokemon Name" />
+              <Form.Text className="text-muted font-expletus">
+                A typo or two are fine ;)
             </Form.Text>
-              </Form.Group>
-              <Button variant="primary" type="button">
-                Search
+            </Form.Group>
+            <Button variant="primary" type="button">
+              Search
             </Button>
-            </Form>
-          </div>
-        </Jumbotron>
-
-        <h2 className="font-expletus">or here's your lucky trio :)</h2>
-
-        <div className="my-5 d-flex justify-content-center" style={{ width: "100%" }}>
-          <CardDeck>
-            <Card className="x-card">
-              <Card.Img id="img-poke-1" className="x-card-img" variant="top" src={this.state.poke1Img} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This content is a little bit longer.
-      </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card className="x-card">
-              <Card.Img id="img-poke-2" variant="top" src={this.state.poke2Img} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-                </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-            <Card className="x-card">
-              <Card.Img id="img-poke-3" variant="top" src={this.state.poke3Img} />
-              <Card.Body>
-                <Card.Title>Card title</Card.Title>
-                <Card.Text>
-                  This is a wider card with supporting text below as a natural lead-in to
-                  additional content. This card has even longer content than the first to
-                  show that equal height action.
-      </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </Card.Footer>
-            </Card>
-          </CardDeck>
+          </Form>
         </div>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
-          crossorigin="anonymous"
-        />
+      </Jumbotron>
+
+      <h2 className="font-expletus">or here's your lucky trio :)</h2>
+
+      <div className="my-5 d-flex justify-content-center" style={{ width: "100%" }}>
+        <CardDeck>
+          <Card className="x-card">
+            <Card.Img id="img-poke-1" className="x-card-img" variant="top" src={poke1Img} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This content is a little bit longer.
+      </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card className="x-card">
+            <Card.Img id="img-poke-2" className="x-card-img" variant="top" src={poke2Img} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card className="x-card">
+            <Card.Img id="img-poke-3" className="x-card-img" variant="top" src={poke3Img} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+      </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
       </div>
-    );
-  }
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+        crossorigin="anonymous"
+      />
+    </div>
+  );
+  // }
 }
 
 export default App;
